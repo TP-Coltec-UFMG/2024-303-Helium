27b5800b5bea9ba19dd8f2b665ed85a7d241240f
Pedro H. <pedro.26.3.h@gmail.com>
2024-05-19 23:16:30 -0300

    Create main menu, architect hierarchy and buttons & document project.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    private GameObject mainMenu;
+    private GameObject optionsMenu;
+    private GameObject creditsMenu;
+    private GameObject colorBlindnessMenu;
+    
+    void Start()
+    {
+        mainMenu = transform.Find("MainMenu").gameObject;
+        optionsMenu  = transform.Find("OptionsMenu").gameObject;
+        creditsMenu = transform.Find("CreditsMenu").gameObject;
+        colorBlindnessMenu = optionsMenu.transform.Find("ColorBlindnessMenu").gameObject;
+    }
+    public void PressStart()
+    {
+        Debug.Log("Play signaled");
+        // SceneManager.LoadScene("");
+    }
+    /* Sistema de toggle (alternar os valores de qual menu está aberto) */
+    public void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void OpenCredits()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+    public void OpenColorBlindnessMenu()
+    {
+        optionsMenu.SetActive(false);
+        colorBlindnessMenu.SetActive(true);
+    }
+    public void Back_ToMenu()
+    {
+        optionsMenu.SetActive(false); creditsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void Back_ToOptions()
+    {
+        colorBlindnessMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Exit signaled");
+        Application.Quit();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# 2024-303-Parkour
+
 # Parkour (nome provisório)
+
+<!-- Thumb
+<img src=".media/thumbnail.png"><br>
+-->
+
+## Sobre
+
+<!-- Screenshot da gameplay
+<p align="center">
+    <img src=".media/screenshot.png">
+</p>
+-->
+
+### Menu inicial
+
+<!-- Screenshot do menu
+<p align="center">
+    <img src=".media/menu.png">
+</p>
+-->
+O menu, voltado para a acessibilidade, consiste em...
+
+## Feito por
+- [Arif Drumond](https://github.com/drumon-iq);
+- [John Wesley](https://github.com/JohnBaska);
+- [Ketelin Luana](https://github.com/Ketelinluana);
+- [Matheus Telles](https://github.com/matheus-telles); e
+- [Pedro Henrique](https://github.com/devwannabe-dot).
